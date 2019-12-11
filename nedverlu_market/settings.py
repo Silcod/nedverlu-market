860816bc45efc93cf2406206efe930788bb70f52
Ibrahim Sanusi <ibrahimsanusi360@yahoo.com>
2019-12-11 11:21:48 +0100

    Updated Settings

--- nedverlu_market/settings.py
+++ nedverlu_market/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'a3m10_*vz&ud=#gn3wr&hp^_@3*q+^33t8y*ffeswh6pioq02@'
 #PRODUCTION_SECRET_KEY = os.environ.get('NEDVERLU_PROSK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['nedverlu.herokuapp.com']
 
