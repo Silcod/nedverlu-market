d7b4c6923f4e10140e6eca278a16ec78e7151b0f
Ibrahim Sanusi <ibrahimsanusi360@yahoo.com>
2019-12-11 22:31:06 +0100

    updated settings

--- core/views.py
+++ core/views.py
@@ -16,6 +16,13 @@ from paystack.utils import get_js_script
 import random
 import string
 import paystack
+import logging
+
+#logger = logging.getLogger(__name__)
+#def my_view(request, arg1, arg):
+ #   if bad_mojo:
+        # Log an error message
+ #       logger.error('Something went wrong!')
 
 
 def create_ref_code():
