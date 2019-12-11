b75f1e92a9722e2f78d3dc5b925ef1248dbcef01
Ibrahim Sanusi <ibrahimsanusi360@yahoo.com>
2019-12-11 13:07:15 +0100

    Updated Settings

--- nedverlu_market/settings.py
+++ nedverlu_market/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'a3m10_*vz&ud=#gn3wr&hp^_@3*q+^33t8y*ffeswh6pioq02@'
 #PRODUCTION_SECRET_KEY = os.environ.get('NEDVERLU_PROSK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
+
 
 ALLOWED_HOSTS = ['nedverlu.herokuapp.com']
 
@@ -171,15 +171,15 @@ PAYSTACK_WEBHOOK_DOMAIN = 'tuteria.ngrok.io'
 
 #Email Config
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ibrahimsanusiwizy@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
 
 
 #For Contact Us
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # For django allauth
 ACCOUNT_EMAIL_REQUIRED = True
